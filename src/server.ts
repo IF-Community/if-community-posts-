b0ad9ba1f232ca1d0b8d25433e436773b14e58c5
kennedywc <kennedy.costan10@gmail.com>
2024-05-11 21:36:11 -0300

    ✨ Initial typeorm configuration

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,31 @@ import express from "express";
 import { router } from "./routes";
 import helmet from "helmet";
 import cors from "cors";
+import AppDataSource from './database/data-source';
 
 
-const app = express();
+AppDataSource.initialize().then(() => {
+    const app = express();
     
-app.use(express.json());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(cors());
 
-app.use(helmet());
-app.use(cors());
+    app.use(router);
+    
+    const PORT_API = Number(process.env.PORT_API ?? '3000');
 
-app.use(router);
-
-const PORT_API = Number(process.env.PORT_API ?? '3000');
-
-const server = app.listen(PORT_API, () => {
-    console.log(`=-=-=-=-=-=-=-=- SERVIDOR INICIADO 🚀 =-=-=-=-=-=-=-=`);
-    console.log(`PORTA: ${PORT_API}`);
-    console.log(`URL: http://localhost:${PORT_API}`);
-    console.log(`=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=`);
-});
+    const server = app.listen(PORT_API, () => {
+        console.log(`=-=-=-=-=-=-=-=- SERVIDOR INICIADO 🚀 =-=-=-=-=-=-=-=-=`);
+        console.log(`PORTA: ${PORT_API}`);
+        console.log(`URL: http://localhost:${PORT_API}`);
+        console.log(`\nDATABASE:`);
+        console.log(`NOME: ${process.env.BD_USERNAME}`);
+        console.log(`PORTA: ${process.env.PORT}`);
+        console.log(`=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=`);
+    });
+  
+    return server;
+}).catch((error) => {
+    console.error("Erro ao inicializar o banco de dados:", error);
+})
