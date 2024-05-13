13167d48228bb131d683b5ae0eba717a6dd8f8d1
kennedywc <kennedy.costan10@gmail.com>
2024-05-13 09:43:06 -0300

    ✨ Routes and operations to manipulate users

---
+++ src/controllers/users/users.controller.ts
@@ -0,0 +1,87 @@
+import AppDataSource from '../../database/data-source';
+import { User } from '../../database/entity/users';
+import { PostgresError } from "pg-error-enum";
+import { ApiError } from '../../helpers/api-error';
+import { StatusCodes } from 'http-status-codes';
+
+export class UsersController {
+    private userRepository = AppDataSource.getRepository(User);
+
+    async create(userData: Partial<User>): Promise<User | void> {
+        try {
+            return await this.userRepository.save(userData);
+        } catch (error: any) {
+
+            if (error.code === PostgresError.UNIQUE_VIOLATION) {
+                throw new ApiError(
+                    'Já existe um usuário com este ID de comunidade.',
+                    StatusCodes.CONFLICT
+                );
+            }
+
+        }
+    }
+    
+
+    async findOne(communitId: number): Promise<User> {
+
+        const user = await this.userRepository.findOne({
+            where: {
+                communitId: communitId
+            },
+        });
+
+        if(!user){
+            throw new ApiError(
+                'Nem um usuário com o communitId informado está cadastrado no sistema',
+                StatusCodes.NOT_FOUND
+            )
+        }
+
+        return user;
+    }
+
+    async find(): Promise<User[]> {
+        return await this.userRepository.find();
+    }
+
+    async update(id: number, userData: Partial<User>): Promise<User> {
+        const { name } = userData;
+        const user = await this.userRepository.findOne({
+            where: { id: id }
+        });
+
+        if(!user){
+            throw new ApiError(
+                'Nem um usuário com o id informado está cadastrado no sistema',
+                StatusCodes.NOT_FOUND
+            )
+        }
+
+        user.name = name ?? user.name;
+
+        user.updatedAt = new Date();
+
+        return await this.userRepository.save(user);
+    }
+
+    async remove(id: number): Promise<User> {
+
+        const userExists = await this.userRepository.exists({
+            where: { id }
+        });
+
+        if (!userExists) {
+            throw new ApiError(
+                'Nenhum usuário com o ID informado está cadastrado no sistema.',
+                StatusCodes.NOT_FOUND
+            );
+        }
+
+        const remove = await this.userRepository.softRemove({
+            id: id
+        });
+        return remove;
+    }
+
+}
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response } from 'express';
+
+import { UsersController } from '../controllers/users/users.controller'
+import { StatusCodes } from 'http-status-codes';
+const userController = new UsersController();
+
+const userRoutes = Router();
+
+userRoutes.post('/users', async (req: Request, res: Response) => {
+    const { body } = req;
+    const user = await userController.create(body);
+    return res.status(StatusCodes.CREATED).json(user);
+});
+
+userRoutes.get('/users', async (req: Request, res: Response) => {
+    const user = await userController.find();
+    return res.status(StatusCodes.OK).json(user);
+});
+
+userRoutes.get('/users/:id', async (req: Request, res: Response) => {
+    const { id } = req?.params;
+    const user = await userController.findOne(+id);
+    return res.status(StatusCodes.OK).json(user);
+});
+
+userRoutes.put('/users/:id', async (req: Request, res: Response) => {
+    const { body } = req;
+    const { id } = req?.params;
+    const user = await userController.update(+id, body);
+    return res.status(StatusCodes.CREATED).json(user);
+});
+
+userRoutes.delete('/users/:id', async (req: Request, res: Response) => {
+    const { id } = req?.params;
+    const user = await userController.remove(+id);
+    return res.status(StatusCodes.OK).json(user);
+});
+
+export { userRoutes };
